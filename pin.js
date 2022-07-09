8cadfca0ad0405e309a72e30ed5e13b2a0180077
Himanshu Soni <57286608+h1m4n3hu@users.noreply.github.com>
2022-07-09 17:14:39 +0530

    Add files via upload

---
+++ pin.js
@@ -0,0 +1,2 @@
+const adsites = ["*://*.doubleclick.net/*","*://partner.googleadservices.com/*","*://*.googlesyndication.com/*","*://*.google-analytics.com/*","*://creative.ak.fbcdn.net/*","*://*.adbrite.com/*","*://*.exponential.com/*","*://*.quantserve.com/*","*://*.scorecardresearch.com/*","*://*.zedo.com/*",]
+chrome.webRequest.onBeforeRequest.addListener(function(details) { return { cancel: true }},{ urls: adsites },["blocking"])
